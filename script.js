4d30f1a9fbb057bc59edb6f56f6726a4004f4970
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2022-12-20 08:37:36 -0500

    Add funtions to script.js for closing menu

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let menuBox= document.getElementById("menu");
 let menuBar= document.getElementById("menu_bar");
 let closingBtn = document.getElementById("btn");
+let menuItems = document.getElementsByClassName("h-menu");
+
 menuBar.onclick= function(){
     menuBox.style.display = "block";
-    menuBox.classList.toggle("open-menu")
+    menuBox.classList.toggle("open-menu");
+}
+
+menuItems.onclick= function(){
+    menuBox.style.display = "none";
+    menuBox.classList.toggle("open-menu");
+}
+
+closingBtn.onclick= function(){
+    menuBox.style.display = "none";
+    menuBox.classList.toggle("open-menu");
 }
