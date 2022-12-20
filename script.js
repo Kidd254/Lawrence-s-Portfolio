cb4c45dacf7aa02ee0865701f3b6fc03951b7ce1
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2022-12-20 10:55:33 -0500

    Add some changes to styles and functions

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ let menuBox= document.getElementById("menu");
 let menuBar= document.getElementById("menu_bar");
 let closingBtn = document.getElementById("btn");
 let menuItems = document.getElementsByClassName("h-menu");
+let menuContainer = document.getElementsByClassName("menu-items");
 
 menuBar.onclick= function(){
-    menuBox.style.display = "block";
+    menuBox.style.display = "flex";
     menuBox.classList.toggle("open-menu");
     menuBar.style.display= "none";
     closingBtn.style.display="block";
+    menuContainer[0].style.display="block";
 }
 
 for(let i=0; i<menuItems.length; i++){
     menuItems[i].onclick= function(){
         menuBox.style.display = "none";
         menuBox.classList.toggle("open-menu");
+        closingBtn.style.display="none";
+        menuBar.style.display= "block";
+        menuContainer[0].style.display="none";
     }
 }
 
@@ -22,5 +27,6 @@ closingBtn.onclick= function(){
     menuBox.classList.toggle("open-menu");
     menuBar.style.display= "block";
     closingBtn.style.display="none";
+    menuContainer[0].style.display="none";
 }
 
