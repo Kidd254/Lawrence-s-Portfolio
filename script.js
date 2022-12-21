2e62cd22837570625eb38b79f13958e872b25011
mastercoder <lorenkioko@gmail.com>
2022-12-21 17:29:15 +0300

    fix linter errors

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let menuBox= document.getElementById("menu");
-let menuBar= document.getElementById("menu_bar");
-let closingBtn = document.getElementById("btn");
-let menuItems = document.getElementsByClassName("h-menu");
-let menuContainer = document.getElementsByClassName("menu-items");
+/* eslint-disable linebreak-style */
+const menuBox= document.getElementById('menu');
+const menuBar= document.getElementById('menu_bar');
+const closingBtn = document.getElementById('btn');
+const menuItems = document.getElementsByClassName('h-menu');
+const menuContainer = document.getElementsByClassName('menu-items');
 
-menuBar.onclick= function(){
-    menuBox.style.display = "flex";
-    menuBox.classList.toggle("open-menu");
-    menuBar.style.display= "none";
-    closingBtn.style.display="block";
-    menuContainer[0].style.display="block";
+menuBar.onclick= function() {
+  menuBox.style.display = 'flex';
+  menuBox.classList.toggle('open-menu');
+  menuBar.style.display= 'none';
+  closingBtn.style.display='block';
+  menuContainer[0].style.display='block';
+};
+
+for (let i=0; i<menuItems.length; i++) {
+  menuItems[i].onclick= function() {
+    menuBox.style.display = 'none';
+    menuBox.classList.toggle('open-menu');
+    closingBtn.style.display='none';
+    menuBar.style.display= 'block';
+    menuContainer[0].style.display='none';
+  };
 }
 
-for(let i=0; i<menuItems.length; i++){
-    menuItems[i].onclick= function(){
-        menuBox.style.display = "none";
-        menuBox.classList.toggle("open-menu");
-        closingBtn.style.display="none";
-        menuBar.style.display= "block";
-        menuContainer[0].style.display="none";
-    }
-}
-
-closingBtn.onclick= function(){
-    menuBox.style.display = "none";
-    menuBox.classList.toggle("open-menu");
-    menuBar.style.display= "block";
-    closingBtn.style.display="none";
-    menuContainer[0].style.display="none";
-}
-
+closingBtn.onclick= function() {
+  menuBox.style.display = 'none';
+  menuBox.classList.toggle('open-menu');
+  menuBar.style.display= 'block';
+  closingBtn.style.display='none';
+  menuContainer[0].style.display='none';
+};
