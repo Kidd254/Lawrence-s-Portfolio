ba70e1f7a8e7b994706d00194fa769007c7a2a06
mastercoder <lorenkioko@gmail.com>
2023-01-05 18:25:10 +0300

    fix pop for section 1

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const menuBar= document.getElementById('menu_bar');
 const closingBtn = document.getElementById('btn');
 const menuItems = document.getElementsByClassName('h-menu');
 const menuContainer = document.getElementsByClassName('menu-items');
-const popButton= document.getElementById('pop-btn');
-popButton.addEventListener('click', ()=>{
-popUp(sectionsCard[2]);
-});
+
 
 menuBar.addEventListener('click', ()=>{
   menuBox.style.display = 'flex';
@@ -35,51 +32,57 @@ closingBtn.addEventListener('click', ()=>{
   menuContainer[0].style.display='none';
 });
 
+const popDisplay= document.getElementById('pop');
+
+popDisplay.addEventListener('click', ()=>{
+popUp(sectionsCard[0]);
+});
 const sections = document.querySelector('.sections');
 let pop = "";
 const sectionsCard = [
   {
     title: 'Tonic',
-    description:
-        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+    list: ['Canopy', 'Back end Dev', '2015'],
+    description:'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     technology: ['html', 'css', 'javascript'],
     liveLink: 'https://kidd254.github.io/Lawrence-s-Portfolio/',
     sourceLink: 'https://github.com/Kidd254/Lawrence-s-Portfolio',
-    images: "Images/Snapshoot Portfolio.png",
+    image: '/images/Snapshoot Portfolio.png',
   },
 
   {
     title: 'Multi-Post Stories',
+    list: ['Canopy', 'Back end Dev', '2015'],
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     technology: ['html', 'css', 'javascript'],
     liveLink: 'https://kidd254.github.io/Lawrence-s-Portfolio/',
     sourceLink: 'https://github.com/Kidd254/Lawrence-s-Portfolio',
-    images: "Images/Snapshoot Portfolio.png",
+    image: './images/Snapshoot Portfolio1.png',
   },
   {
     title: 'Tonic',
+    list: ['Canopy', 'Back end Dev', '2015'],
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     technology: ['html', 'css', 'javascript'],
     liveLink: 'https://kidd254.github.io/Lawrence-s-Portfolio/',
     sourceLink: 'https://github.com/Kidd254/Lawrence-s-Portfolio',
-    images: "Images/Snapshoot Portfolio.png",
+    image: './images/Snapshoot Portfolio2.png',
   },
   {
     title: 'Multi-Post Stories',
+    list: ['Canopy', 'Back end Dev', '2015'],
     description:
         'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
     technology: ['html', 'css', 'javascript'],
     liveLink: 'https://kidd254.github.io/Lawrence-s-Portfolio/',
     sourceLink: 'https://github.com/Kidd254/Lawrence-s-Portfolio',
-    images: "Images/Snapshoot Portfolio.png",
-
+    image: './images/Snapshoot Portfolio3.png',
   },
 ];
 
 const popUp = (info) => {
-  debugger;
   const popUpContainer = document.createElement('section');
   popUpContainer.id = "pop-up-main";
   popUpContainer.innerHTML = `<div class="pop-content">
@@ -88,23 +91,25 @@ const popUp = (info) => {
       <button id="closing-btn-pop">
         <i class="fa-solid fa-xmark menu-bar" id="btn"></i>
       </button>
+      <ul>
+      <li>Canopy</li>
+      <li>Back End Dev</li>
+      <li>2015</li>
+      </ul>
     </div>
-    <div class="pop-up-image"><img src="${info.images}"></div>
+    <div class="pop-up-image"><img src="${info.image}"></div>
     <div class="pop-up-content">
       <p class="pop-up-description">${info.description}</p>
       <ul class="">
+      </p>
+    <ul class="category">
       <li>${info.technology[0]}</li>
       <li>${info.technology[1]}</li>
       <li>${info.technology[2]}</li>
+    </ul>
       </ul>
     </div>
-    <div class="pop-up-buttons">
-    <p class="title" hidden>${info.title}</p>
-    <p class="description" hidden>${info.description}</p>
-    <p class="livelink" hidden>${info.liveLink}</p>
-    <p class="sourcelink" hidden>${info.sourceLink}</p>
-    <button class="btn1" id="w2button">See Project</button>
-    </div>
+    <div class="pop-up-buttons"></div>
   </div>
   `;
 
@@ -115,67 +120,3 @@ const popUp = (info) => {
     popUpContainer.remove();
   })
 };
-
-/*sectionsCard.forEach((data) => {
-  pop += `<div id="w2" class="work1">
-  <div class="work1Discription" id="w2work1Discription">
-    <h2>
-      <span id="w2d">${data.title}</span>
-    </h2>
-    <button id="closing-btn-pop">
-        <i class="fa-solid fa-xmark menu-bar" id="btn"></i>
-      </button>
-    <div class="pop-up-image">${data.image}</div>
-    <p>
-    ${data.description}
-    </p>
-    <ul class="category">
-      <li>${data.technology[0]}</li>
-      <li>${data.technology[1]}</li>
-      <li>${data.technology[2]}</li>
-    </ul>
-  </div>
-  <p class="title" hidden>${data.title}</p>
-  <p class="description" hidden>${data.description}</p>
-  <p class="livelink" hidden>${data.liveLink}</p>
-  <p class="sourcelink" hidden>${data.sourceLink}</p>
-  <button class="btn1" id="w2button">See Project</button>
-</div>`;
-  sections.innerHTML = pop;
-});
-
-const container = document.querySelector('.sections');
-const recent = document.querySelector('.Recentwork1');
-const closeIcon = document.querySelector('.crossimg');
-const css = document.querySelector('.css-display');
-const multiProject = document.querySelector('.button');
-const title = document.querySelector('.recent-title');
-const description = document.querySelector('.recent-description');
-const liveLink = document.querySelector('.live');
-const sourceLink = document.querySelector('.source');
-
-const button = container.querySelectorAll('.btn1');
-const recentTitle = container.querySelectorAll('.title');
-const recentDescription = container.querySelectorAll('.description');
-const recentlivelink = container.querySelectorAll('.livelink');
-const recentsourcelink = container.querySelectorAll('.sourcelink');
-
-for (let i = 0; i < button.length; i += 1) {
-  button[i].addEventListener('click', () => {
-    title.textContent = recentTitle[i].innerHTML;
-    description.textContent = recentDescription[i].innerHTML;
-    liveLink.sourceLink = recentlivelink[i].outerHTML;
-    sourceLink.sourceLink = recentsourcelink[i].outerHTML;
-
-    recent.classList.add('active');
-  });
-}
-closeIcon.addEventListener('click', () => {
-  css.classList.remove('show');
-  recent.classList.remove('active');
-});
-
-multiProject.addEventListener('click', () => {
-  css.classList.add('show');
-  recent.classList.add('active');
-});*/
