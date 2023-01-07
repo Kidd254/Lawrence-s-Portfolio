8c7451f4694851c214df74e0d06b6a69efd5a22c
mastercoder <lorenkioko@gmail.com>
2023-01-07 16:59:08 +0300

    fix linters

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line linebreak-style
-// eslint-disable-next-line linebreak-style
 /* eslint-disable require-jsdoc */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
